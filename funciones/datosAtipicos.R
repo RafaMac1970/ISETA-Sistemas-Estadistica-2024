5da3cd9ceeca95ec503f679f7de2594664d67c50
RafaMac1970 <rafamacdonough@gmail.com>
2024-05-03 12:48:37 -0300

    Actualizacion tendencia central y varianza

--- funciones/datosAtipicos.R
+++ funciones/datosAtipicos.R
@@ -1,6 +1,6 @@
 datosAtipicos <- function (aVector) {
-  irq <- q3(aVector) - q1(aVector)
-  min <- q1(aVector) - 1.5 * irq
-  max <- q3(aVector) + 1.5 * irq
+  riq <- q3(aVector) - q1(aVector) # riq viene de rango inter quiartil
+  min <- q1(aVector) - 1.5 * riq
+  max <- q3(aVector) + 1.5 * riq
   aVector[aVector < min | aVector > max]
 } 
